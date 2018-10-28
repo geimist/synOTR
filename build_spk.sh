994001c2a68dfccc80b43f233c404550f1ce09bb
Stephan Geisler <synotr@geimist.eu>
2018-10-28 16:04:15 +0000

    „build_spk.sh“ ändern

--- build_spk.sh
+++ build_spk.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-# Dieses Script cloned das synOTR-Repository, passt die Dateirechte an und erstellt ein SPK
+# Dieses Script clont das synOTR-Repository, passt die Dateirechte an und erstellt ein SPK
+
+echo "Dieses Skript befindet sich noch im Aufbau und steht noch nicht zur Verfügung!"
 
 # git clone https://geimist.eu:30443/geimist/synOTR.git
